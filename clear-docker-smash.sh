34926c4a79c56630badddb099d4aca4322fee02a
AoJ <aoj@n13.cz>
2014-05-12 13:50:16 +0200

    changes

--- clear-docker-smash.sh
+++ clear-docker-smash.sh
@@ -1,2 +1,12 @@
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+# We're being careful here in that we're only cleaning up containers which are no longer running
+docker ps -a | grep "Exit" | awk '{print $1}' | while read -r id ; do
+  sudo docker rm $id
+done
+
+# We only remove images which don't have a name
+sudo docker images | grep "^<none>" | head -n 1 | awk 'BEGIN { FS = "[ \t]+" } { print $3 }'  | while read -r id ; do
+  sudo docker rmi $id
+done
+
+# docker rm $(docker ps -a -q)
+# docker rmi $(docker images -q)
--- zapa-worker-local/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#!/bin/bash
-
-
-/usr/bin/supervisord
